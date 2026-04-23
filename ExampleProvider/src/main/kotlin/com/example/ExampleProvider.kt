ad9130f24755a71fd7e8cad8595aa20fb8f1d90f
Melih Sinan <msdogrul1@gmail.com>
2026-04-23 15:12:18 +0300

    Test

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.TvType
 
 class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
     override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+    override var name = "Example provider Test"
     override val supportedTypes = setOf(TvType.Movie)
 
     override var lang = "en"
