f939a829e6f25a744dafb9986f8fb4267b3cbb88
Melih Sinan <msdogrul1@gmail.com>
2026-04-23 17:12:59 +0300

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "saflisgit/melihcs3")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/saflisgit/melihcs3")
     }
 
     android {
