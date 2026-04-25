f3665a33932ac791df4c38b2c003bc77f81d4f78
Melih Sinan <msdogrul1@gmail.com>
2026-04-26 00:03:51 +0300

    test

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -10,7 +10,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove any of them.
 
     description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+    authors = listOf("melih")
 
     /**
     * Status int as one of the following:
--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,67 @@
 package com.example
 
+import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.addDubStatus
+import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.newAnimeSearchResponse
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.syncproviders.providers.OpenSubtitlesApi.Companion.headers
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider Test1"
-    override val supportedTypes = setOf(TvType.Movie)
+class ExampleProvider : MainAPI() { 
+    override var mainUrl = "https://filmmakinesi.to"
+    override var name = "Film Makinesi"
+    override val supportedTypes = setOf(TvType.TvSeries, TvType.Movie)
 
-    override var lang = "en"
+    override var lang = "tr"
 
-    // Enable this when your provider has a main page
+    override val mainPage = mainPageOf(
+        "" to "Anasayfa",
+        "en-yeni-filmler" to "En Yeni Filmler",
+        "en-cok-izlenen-filmler" to "En Çok İzlenen Filmler"
+    )
+
     override val hasMainPage = true
 
-    // This function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf()
+    }
+
+    override suspend fun getMainPage(
+        page: Int,
+        request : MainPageRequest
+    ): HomePageResponse {
+        // Build the URL based on page
+        val url = if (page == 1) {
+            if (request.data.isEmpty()) mainUrl else "$mainUrl/${request.data}"
+        } else {
+            if (request.data.isEmpty()) "$mainUrl/page/$page" else "$mainUrl/${request.data}/page/$page"
+        }
+        
+        val doc = app.get(url).document
+        
+        // Use generic selectors that are common on movie sites, or specific ones if known
+        val elements = doc.select("div.poster, div.movie-poster, div.movie-box, article, .item, .post, .movie")
+        
+        val home = elements.mapNotNull { element ->
+            val a = element.selectFirst("a") ?: return@mapNotNull null
+            val href = a.attr("href")
+            if (href.isEmpty() || href == "#") return@mapNotNull null
+            
+            val img = element.selectFirst("img")
+            val title = img?.attr("alt")?.ifEmpty { img.attr("title") } ?: a.attr("title").ifEmpty { a.text() }
+            val posterUrl = img?.attr("data-src")?.ifEmpty { img.attr("src") }
+            
+            if (title.isBlank()) return@mapNotNull null
+            
+            com.lagradost.cloudstream3.newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = posterUrl
+            }
+        }.distinctBy { it.url }
+
+        return newHomePageResponse(request.name, home)
     }
 }
